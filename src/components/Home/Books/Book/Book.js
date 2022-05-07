7a3997b9fdf7ad794d153f91dd0bad4f4b0a10c5
MDShakibul <sakib6610@gmail.com.com>
2022-05-08 03:34:47 +0600

    all books show

--- src/components/Home/Books/Book/Book.js
+++ src/components/Home/Books/Book/Book.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import './Book.css'
 
-const Book = () => {
+const Book = ({book}) => {
+    const {img, name, description, price, quantity, supplier} = book;
     return (
-        <div>
-        <h1>This is book</h1>
-        </div>
+            <div className="col-lg-4 clo-sm-12">
+                <div className="p-3 border bg-light">
+                    <div className='book-img'>
+                        <img src={img} alt=""/>
+                    </div>
+                    <h4 className='mt-3'>Name: {name}</h4>
+                    <p><strong>Description:</strong> {description}</p>
+                    <p><strong>Price:</strong> {price}</p>
+                    <p><strong>Quantity:</strong> {quantity}</p>
+                    <p><strong>Suplier:</strong> {supplier}</p>
+                    <div className='d-flex justify-content-center'>
+                    <button className='btn-update bg-success text-white px-3 py-2'>Update</button></div>
+                </div>
+            </div>
     );
 };
 
--- src/components/Home/Books/Books.js
+++ src/components/Home/Books/Books.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import useBooks from '../../../hooks/useBooks';
+import Book from './Book/Book';
 
 const Books = () => {
     const[books] = useBooks();
+   /*  {
+        products.slice(0, 6).map(product => <Product
+            key={product._id}
+            product={product}
+        ></Product>)
+    } */
     return (
         <div>
-        <h1>This is books: {books.length}</h1>
+            <h1 className='text-center mt-3 text-primary mb-3'>Book Store</h1>
+
+            <div className='container'>
+                <div className='row g-3'>
+                {
+                    books.slice(0, 6).map(book => <Book
+                        key={book._id}
+                        book={book}
+                    ></Book>)
+                } 
+                </div>
+            </div>
+            <div className='d-flex justify-content-center mt-4'>
+                <button className='btn-update px-4 py-3 bg-primary text-white'>All Books</button>
+            </div>
         </div>
     );
 };
